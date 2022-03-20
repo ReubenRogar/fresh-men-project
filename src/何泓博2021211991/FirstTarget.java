d330eca1a525011eb229d1f03227ec5eac6fb3aa
DYH <1245687900@qq.com>
2022-03-20 23:10:54 +0800

    HHB is pig

--- src/何泓博2021211991/FirstTarget.java
+++ src/何泓博2021211991/FirstTarget.java
@@ -2,4 +2,7 @@ package 何泓博2021211991;
 
 public class FirstTarget {
     /*以这个格式上传一个软件包 输出：“Git搞定！”*/
+    public static void main(String[] args) {
+        System.out.println("HHB is pig!");
+    }
 }
