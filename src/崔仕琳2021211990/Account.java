c0584ad6404ab97c7bf4cf85e2d48ca68cb6da71
sy1y <10615452+sy1y@user.noreply.gitee.com>
2022-03-24 21:19:06 +0800

    .

--- src/崔仕琳2021211990/Account.java
+++ src/崔仕琳2021211990/Account.java
@@ -6,11 +6,6 @@ import java.util.List;
 public class Account {
 
     private String hostName;    //账户用户名
-
-    public String getPassword() {
-        return password;
-    }
-
     private String password;    //账户登陆密码
     private String idNumber;    //账户人身份证号
     private List<Card> cards = new ArrayList<>();   //此账户下拥有的卡对象
