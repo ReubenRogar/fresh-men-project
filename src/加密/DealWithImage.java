4e19b5204a02ec81d33be0da932a72ea7627cac7
ReubenRogar <3186664089@qq.com>
2022-04-10 19:35:43 +0800

    提取dct的数据

--- src/加密/DealWithImage.java
+++ src/加密/DealWithImage.java
@@ -1,33 +1,32 @@
 package 加密;
 
+import java.util.ArrayList;
+
 public class DealWithImage {
     public static byte[] xorCode(byte[] code){
         double u = 3.79,x = 0.88;
-        for(int i = 0 ;i < code.length-2;i++){
-            if(code[i] == -1&&code[i+1] == -38){
-                i+=14;
-                    while (i<code.length-2) {
+        for(int i = 0 ;i < code.length;i++){
                         if(code[i] != -1){
-                            code[i] = (byte) ((int) code[i] ^ (int) (x * 127));
-                            x = u * x*(1 - x);
-                            i++;
+                            code[i] =(byte) ((int) code[i] ^ (int) (x * 127));
+                            x = u * x * (1 - x);
                         }
-                    }
-
-            }
         }
         return code;
     }
-    public static void main(String[] args) {
-        /*
-        byte[] a = {127,68,0,1,2,3,4,5,6,127};
-        for (byte b : a) {
-            System.out.print(b+" ");
+
+    public static byte[] getImageCode(byte[] code){
+        int i;
+        for (i =0;i <code.length;i++){
+            if(code[i] == -1&&code[i+1] == -38) {
+                i += 2;
+                break;
+            }
         }
-        System.out.println();
-        for (byte b : xorCode(a)) {
-            System.out.print(b+" ");
+        i += code[i] * 16 * 16 +code[i+1];
+        byte[] target = new byte[code.length-2-i];
+        for(int j = 0;j < target.length;j++){
+            target[j] = code[j+i];
         }
-    }*/
+        return target;
     }
 }
--- src/加密/TextDemo.java
+++
@@ -1,5 +0,0 @@
-package 加密;
-
-public class TextDemo {
-
-}
