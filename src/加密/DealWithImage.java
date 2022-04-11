e08a60f30d1281670faa032719a862964f907040
ReubenRogar <3186664089@qq.com>
2022-04-11 22:35:16 +0800

    完成changeDC方法3

--- src/加密/DealWithImage.java
+++ src/加密/DealWithImage.java
@@ -9,6 +9,7 @@ public class DealWithImage {
     public static final DCTable DCC;
     public static final ACTable ACL;
     public static final ACTable ACC;
+    private static double u = 3.79, x = 0.88;
 
     static {
         // 直流亮度表
@@ -20,7 +21,7 @@ public class DealWithImage {
         // 交流色度表
         ACC = new ACTable("./HuffmanTable/AC_chrominance.txt");
     }
-    public static String getDCL(String code,DCTable dcs){
+    public static String getDC(String code,DCTable dcs){
         int[] dcsLength = dcs.getCategory(code);
         return code.substring(dcsLength[1]-1,dcsLength[0]+dcsLength[1]);
     }
@@ -31,20 +32,10 @@ public class DealWithImage {
      * @param code 字节码
      * @return 异或后字节码
      */
-    public static byte[] xorCode(byte[] code) {
-        double u = 3.79, x = 0.88;
-        for (int i = 0; i < code.length; i++) {
-            if (code[i] != -1) {
-                code[i] = (byte) ((int) code[i] ^ (int) (x * 1024));
-                x = u * x * (1 - x);
-            }
-        }
-        return code;
-    }
 
-    public static int xorCode(int code) {
-        double u = 3.79, x = 0.88;
-        code = code ^ (int) (x * 1024);
+    public static int xorCode(int code,int length) {
+        int xorTarget = (int)((Math.pow(2,length)-Math.pow(2,length-1))*x +Math.pow(2,length-1));
+        code = code ^ xorTarget;
         x = u * x * (1 - x);
         return code;
     }
@@ -53,35 +44,27 @@ public class DealWithImage {
         int DC = 0, delta = 1;
         byte DCs[] = DCBefore.getBytes();
         String result ="";
-        if (DCs[0] == '0') {//DC差分为负数
-            for (int i = DCs.length - 1; i >= 0; i--) {
-                if (DCs[i] == '0') DC += delta;
-                delta *= 2;
-            }
-            DC = xorCode(DC);
-            LinkedList<Character> DCAfter = new LinkedList<>();
-            while (DC > 0) {
-                DCAfter.addFirst((char)(DC%2==0?'1':'0'));
-                DC /= 2;
-            }
-            for (Character character : DCAfter) {
-                result += character;
-            }
-        } else {//DC差分为正数
             for (int i = DCs.length - 1; i >= 0; i--) {
                 if (DCs[i] == '1') DC += delta;
                 delta *= 2;
             }
-            DC = xorCode(DC);
+            System.out.println(DC);
+            DC = xorCode(DC,DCBefore.length());
             LinkedList<Character> DCAfter = new LinkedList<>();
             while (DC > 0) {
                 DCAfter.addFirst((char)(DC%2+'0'));
                 DC /= 2;
             }
+            if(DCAfter.size()< DCs.length)DCAfter.addFirst('0');
             for (Character character : DCAfter) {
                 result += character;
             }
-        }
         return result;
+    }
+
+    public static void main(String[] args) {
+        String text ="0100101";
+        String text1 ="1011010";
+        System.out.println(changeDC(text));
     }
 }
