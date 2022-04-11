1f915e858086b90550addb9b318b02d915083ec8
ReubenRogar <3186664089@qq.com>
2022-04-11 21:50:21 +0800

    DCTable优化

--- src/加密/DCTable.java
+++ src/加密/DCTable.java
@@ -6,7 +6,7 @@ public class DCTable {
     /**
      * 直流Huffman表
      */
-        private final int[] categoryAndCodeWordLength;
+
         private final int[] codeWordLength;
         private final int[] category;
         private final String[] codeWord;
@@ -15,6 +15,7 @@ public class DCTable {
         public DCTable(String fileName) {
             category = new int[12];
             codeWord = new String[12];
+            codeWordLength = new int[11];
             this.fileName = fileName;
             init();
         }
@@ -30,8 +31,8 @@ public class DCTable {
                     while ((lineContent = br.readLine()) != null) {
                         String[] ss = lineContent.split("\\s\\s");
                         category[index] = Integer.parseInt(ss[0]);
-                        codeWord[index++] = ss[1];
-                        codeWordLength[index++] = ss[2];
+                        codeWord[index] = ss[1];
+                        codeWordLength[index++] = Integer.parseInt(ss[2]);
                     }
                     br.close();
                     fileReader.close();
@@ -46,6 +47,7 @@ public class DCTable {
         }
         //得到DC类别（长度）
         public int[] getCategory(String code){
+            int[] categoryAndCodeWordLength = new int[2];
             int i = 0;
             for(;i < 12;i++){
                 if(code.startsWith(codeWord[i])){
--- src/加密/DealWithImage.java
+++ src/加密/DealWithImage.java
@@ -2,47 +2,71 @@ package 加密;
 
 
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
 
 public class DealWithImage {
     public static final DCTable DCL;
-    public static  final DCTable DCC;
+    public static final DCTable DCC;
     public static final ACTable ACL;
     public static final ACTable ACC;
+
     static {
         // 直流亮度表
-         DCL = new DCTable("./HuffmanTable/DC_luminance.txt");
+        DCL = new DCTable("./HuffmanTable/DC_luminance.txt");
         // 直流色度表
-         DCC = new DCTable("./HuffmanTable/DC_chrominance.txt");
+        DCC = new DCTable("./HuffmanTable/DC_chrominance.txt");
         // 交流亮度表
-         ACL = new ACTable("./HuffmanTable/AC_luminance.txt");
+        ACL = new ACTable("./HuffmanTable/AC_luminance.txt");
         // 交流色度表
-         ACC = new ACTable("./HuffmanTable/AC_chrominance.txt");
+        ACC = new ACTable("./HuffmanTable/AC_chrominance.txt");
     }
     public static String getDCL(String code,DCTable dcs){
         int[] dcsLength = dcs.getCategory(code);
         return code.substring(dcsLength[1]-1,dcsLength[0]+dcsLength[1]);
     }
+
     /**
      * 异或处理二进制数组
+     *
      * @param code 字节码
      * @return 异或后字节码
      */
-    public static byte[] xorCode(byte[] code){
-        double u = 3.79,x = 0.88;
-        for(int i = 0 ;i < code.length;i++){
-                        if(code[i] != -1){
-                            code[i] =(byte) ((int) code[i] ^ (int) (x * 127));
-                            x = u * x * (1 - x);
-                        }
+    public static byte[] xorCode(byte[] code) {
+        double u = 3.79, x = 0.88;
+        for (int i = 0; i < code.length; i++) {
+            if (code[i] != -1) {
+                code[i] = (byte) ((int) code[i] ^ (int) (x * 1024));
+                x = u * x * (1 - x);
+            }
         }
         return code;
     }
 
-    public static String changeDC(String DCbefore){
-        long DC = 0;
-        byte DCs[] = DCbefore.getBytes();
-        if(DCs[0] == '0'){
-            for()
+    public static int xorCode(int code) {
+        double u = 3.79, x = 0.88;
+        code = code ^ (int) (x * 1024);
+        x = u * x * (1 - x);
+        return code;
+    }
+
+    public static String changeDC(String DCBefore) {
+        int DC = 0, delta = 1;
+        byte DCs[] = DCBefore.getBytes();
+        if (DCs[0] == '0') {//DC差分为负数
+            for (int i = DCs.length - 1; i >= 0; i--) {
+                if (DCs[i] == '0') DC += delta;
+                delta *= 2;
+            }
+            LinkedList<Character> DCAfter = new LinkedList<>();
+            delta = 1;
+            while (DC > 0) {
+                DCAfter.addFirst(00);
+            }
+        } else {//DC差分为正数
+            for (int i = DCs.length - 1; i >= 0; i--) {
+                if (DCs[i] == '1') DC += delta;
+                delta *= 2;
+            }
         }
     }
 }
