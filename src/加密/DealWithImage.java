dfe5b945be5b9a4e83a13df58143213ee20dc89a
CAiRuu <1471301157@qq.com>
2022-04-12 23:08:30 +0800

    ok

--- src/加密/DealWithImage.java
+++ src/加密/DealWithImage.java
@@ -50,12 +50,13 @@ public class DealWithImage {
             i += pd.x + pd.y;
             if(flag == 0) {
                 pa = ACL.getRunSize(code.substring(codeTail,l));
-                for( ;pa[0] != 0||pa[1] != 0; ){      //非0/0时
+                while( pa[0] != 0||pa[1] != 0 ) {      //非0/0时
                     i += pa[1] + pa[2];
                     for (int j = 0; j < pa[0]; j++) {       //R
                         arr[k] = 0;
                         k++;
                     }
+                }
                     codeHead = codeTail + pa[2];
                     codeTail = codeHead + pa[1];
                     arr[k] = str0b2int(code.substring(codeHead,codeTail));
@@ -63,20 +64,47 @@ public class DealWithImage {
                     pa = ACL.getRunSize(code.substring(codeTail));
                 }
                 i++;// 0/0,识别码为0，后移一位
+                if(i == l-1) {
+                    for (int j = k; j < 64; j++) {
+                        arr[j] = 0;
+                        DCT.add(arr);
+                        return DCT;
+                    }
+                }
                 codeTail++;
                 if((i+1) % 8 != 0) {
                     codeHead = codeTail + 1;
                     while ((i + 1) % 8 != 0) {//移到字节尾
                         i++;
+                        if(i == l-1) {
+                            for (int j = k; j < 64; j++) {
+                                arr[j] = 0;
+                                DCT.add(arr);
+                                return DCT;
+                            }
+                        }
                         codeHead++;
                     }
                     i++;//跳到下一字节
+                        if(i == l-1) {
+                            for (int j = k; j < 64; j++) {
+                                arr[j] = 0;
+                                DCT.add(arr);
+                                return DCT;
+                            }
                     codeHead++;
                 }
-                else{
-                    codeHead = codeTail + 1;
-                    i++;
-                }
+                else {
+                            codeHead = codeTail + 1;
+                            i++;
+                            if (i == l - 1) {
+                                for (int j = k; j < 64; j++) {
+                                    arr[j] = 0;
+                                    DCT.add(arr);
+                                    return DCT;
+                                }
+                            }
+                        }
                 for(;k < 64 ;k++) arr[k] = 0;
                 DCT.add(arr);
                 flag++;
@@ -101,14 +129,35 @@ public class DealWithImage {
                     codeHead = codeTail + 1;
                     while ((i + 1) % 8 != 0) {//移到字节尾
                         i++;
+                        if (i == l - 1) {
+                            for (int j = k; j < 64; j++) {
+                                arr[j] = 0;
+                                DCT.add(arr);
+                                return DCT;
+                            }
+                        }
                         codeHead++;
                     }
                     i++;//跳到下一字节
+                    if (i == l - 1) {
+                        for (int j = k; j < 64; j++) {
+                            arr[j] = 0;
+                            DCT.add(arr);
+                            return DCT;
+                        }
+                    }
                     codeHead++;
                 }
                 else{
                     codeHead = codeTail + 1;
                     i++;
+                    if (i == l - 1) {
+                        for (int j = k; j < 64; j++) {
+                            arr[j] = 0;
+                            DCT.add(arr);
+                            return DCT;
+                        }
+                    }
                 }
                 for(;k < 64 ;k++) arr[k] = 0;
                 DCT.add(arr);
@@ -295,7 +344,7 @@ public class DealWithImage {
     public static void main(String[] args) {
         int[] ints = new int[64];
         ArrayList<int[]> arr = new ArrayList<>();
-        String code = "11100010 11101000 10100010 10001010 11111001 10010011 11110111 00010011";
+        String code = "1110001011101000101000101000101011111001100100111111011100010011";
         arr = getDCT(code);
         ints = arr.get(1);
         for (int i = 0; i < 64; i++) {
