c5587d9c0c73ddbe82403997db04cbedc1e12c1b
ReubenRogar <3186664089@qq.com>
2022-08-07 07:48:39 +0800

    完成新加密密钥构建

--- src/加密/NewTypeEncrypt.java
+++ src/加密/NewTypeEncrypt.java
@@ -8,14 +8,16 @@ import java.util.ArrayList;
 
 public class NewTypeEncrypt {
     private ArrayList<int[]> DCTs;
-    private Point originKey;
-    private Point finalKey;
+    private KeyXU originKey;
+    private KeyXU finalKey;
 
-    public NewTypeEncrypt(ArrayList<int[]> d,Point k) throws NoSuchAlgorithmException {
+    public NewTypeEncrypt(ArrayList<int[]> d,KeyXU k) throws NoSuchAlgorithmException {
         DCTs = d;
         originKey = k;
 
         int[] mount = new int[64];
+        for(int i = 0;i <64;i++)
+            mount[i] = 0;
         int[] dct;
         for (int i = 0;i < DCTs.size();i++){
             dct = DCTs.get(i);
@@ -31,14 +33,44 @@ public class NewTypeEncrypt {
         }
         MessageDigest digest = MessageDigest.getInstance("SHA-512");
         byte[] encodedHash = digest.digest(feature.getBytes(StandardCharsets.UTF_8));
-        String hashfeature = DealWithImage.bytes2Str0b(encodedHash);
-
+        String hashFeature = DealWithImage.bytes2Str0b(encodedHash);
+        int count;
+        String addXKey = "",addUKey = "";
+        //前半加密x
+        for(int i = 0;i < 28;i++){
+            count = 0;
+            for(int r = 0;r < 9;r++){
+                if(hashFeature.charAt(r) == '1')count++;
+            }
+            hashFeature = hashFeature.substring(9);
+            addXKey += ""+count;
+        }
+        count = 0;
+        for(int i = 0;i < 4;i++){
+            if(hashFeature.charAt(i) == '1')count++;
+        }
+        addXKey += ""+count;
+        hashFeature = hashFeature.substring(4);
+        //后半加密u
+        count = 0;
+        for(int i = 0;i < 4;i++){
+            if(hashFeature.charAt(i) == '1')count++;
+        }
+        addUKey += ""+count;
+        hashFeature = hashFeature.substring(4);
+        for(int i = 0;i < 28;i++){
+            count = 0;
+            for(int r = 0;r < 9;r++){
+                if(hashFeature.charAt(r) == '1')count++;
+            }
+            hashFeature = hashFeature.substring(9);
+            addUKey += ""+count;
+        }
+        finalKey = new KeyXU(  Double.valueOf(""+ originKey.x+addXKey),Double.valueOf(""+ originKey.y+addUKey));
     }
 
-//    public static void main(String[] args) throws NoSuchAlgorithmException {
-//        String feature = "2312312435454632145376541212197378547431414496";
-//        MessageDigest digest = MessageDigest.getInstance("SHA-512");
-//        byte[] encodedhash = digest.digest(feature.getBytes(StandardCharsets.UTF_8));
-//        System.out.println(encodedhash.length);
-//    }
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        
+        System.out.println();
+    }
 }
