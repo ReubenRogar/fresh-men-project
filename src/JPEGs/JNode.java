45273f707ef85428bfc9ceddd155b13f1ffb44e5
CAiRuu <1471301157@qq.com>
2022-09-14 17:11:17 +0800

    .

--- src/JPEGs/JNode.java
+++
@@ -1,7 +0,0 @@
-package JPEGs;
-
-public class JNode {
-    int id;
-    int offset;
-    int lenth;
-}
--- src/cn/hitwh/JNode.java
+++
@@ -1,7 +0,0 @@
-package cn.hitwh;
-
-public class JNode {
-    int id;
-    int offset;
-    int lenth;
-}
