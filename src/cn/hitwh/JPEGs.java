46a19953a649b14a8d34fe99c65bacabea0d3c02
kkkop <1803427906@qq.com>
2022-09-14 21:10:45 +0800

    图片属性获取方法更正

--- src/cn/hitwh/JPEGs.java
+++ src/cn/hitwh/JPEGs.java
@@ -5,7 +5,12 @@ package cn.hitwh;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.stream.FileImageInputStream;
 import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static cn.hitwh.ImageToCode.imageToByte;
@@ -28,85 +33,120 @@ public class JPEGs {
     private byte[] image;
     private byte[] target;
     private int start;
-    private int Heigh;//图片高度
-    private int Width;//图片宽度
-    private int sampling ;//采样系数比例的整数形，如（422 444）
+    private static int Heigh;//图片的高度
+    private static int Width;//图片的宽度
+    private static int Sampling;//图片的采样模式
 
 
     //Logback框架
     public static final Logger LOGGER = LoggerFactory.getLogger("JPEGs.class");
 
+    public byte[] image2byte(String path){
+        byte[] data = null;
+        FileImageInputStream input = null;
+        try {
+            input = new FileImageInputStream(new File(path));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int numBytesRead = 0;
+            while ((numBytesRead = input.read(buf)) != -1) {
+                output.write(buf, 0, numBytesRead);
+            }
+            data = output.toByteArray();
+            output.close();
+            input.close();
+        }
+        catch (FileNotFoundException ex1) {
+            ex1.printStackTrace();
+        }
+        catch (IOException ex1) {
+            ex1.printStackTrace();
+        }
+        return data;
+    }
+    public String byte2string(byte[] data){
+        if(data==null||data.length<=1) return "0x";
+        if(data.length>200000) return "0x";
+        StringBuffer sb = new StringBuffer();
+        int buf[] = new int[data.length];
+        //byte数组转化成十进制
+        for(int k=0;k<data.length;k++){
+            buf[k] = data[k]<0?(data[k]+256):(data[k]);
+        }
+        //十进制转化成十六进制
+        for(int k=0;k<buf.length;k++){
+            if(buf[k]<16) sb.append("0"+Integer.toHexString(buf[k]));
+            else sb.append(Integer.toHexString(buf[k]));
+        }
+        return "0x"+sb.toString().toUpperCase();
+    }
     /**
      * 读取SOF0段数据获取图片信息
      */
-    public void translate(String Jhin) {
+    public SOF0 translate(String Jhin) {
         SOF0 s = new SOF0();
         int offset = 0;
         for (int i = 0; i < Jhin.length(); i++) {
-            if (Jhin.charAt(i) == 'f' && Jhin.charAt(i) == 'f' && Jhin.charAt(i) == 'c' && Jhin.charAt(i) == '0')
+            if (Jhin.charAt(i) == 'F' && Jhin.charAt(i + 1) == 'F' && Jhin.charAt(i + 2) == 'C' && Jhin.charAt(i + 3) == '0') {
                 offset = i;
-            break;
+                break;
+            }
         }
         offset += 4;
         if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z')
-            s.SOF0length = 16 * 16 * 16 * (Jhin.charAt(offset) - 87);
+            s.SOF0length = 16 * 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.SOF0length = 16 * 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
         if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z')
-            s.SOF0length += 16 * 16 * (Jhin.charAt(offset) - 87);
+            s.SOF0length += 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.SOF0length += 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.SOF0length += 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.SOF0length += 16 * (Jhin.charAt(offset) - 55);
         else s.SOF0length += 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.SOF0length += Jhin.charAt(offset) - 87;
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.SOF0length += Jhin.charAt(offset) - 55;
         else s.SOF0length += Jhin.charAt(offset) - 48;//SOF0长度
 
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numColor = 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numColor = 16 * (Jhin.charAt(offset) - 55);
         else s.numColor = 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numColor += Jhin.charAt(offset) - 87;
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numColor += Jhin.charAt(offset) - 55;
         else s.numColor += Jhin.charAt(offset) - 48;//颜色分量
 
         offset++;
         if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z')
-            s.hight = 16 * 16 * 16 * (Jhin.charAt(offset) - 87);
+            s.hight = 16 * 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.hight = 16 * 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.hight += 16 * 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.hight += 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.hight += 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.hight += 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.hight += 16 * (Jhin.charAt(offset) - 55);
         else s.hight += 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') {
-            s.hight += Jhin.charAt(offset) - 87;
-        } else {
-            s.hight += Jhin.charAt(offset) - 48;//得到高度
-        }
-        Heigh = s.hight;//将高度数据存进heigh中
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.hight += Jhin.charAt(offset) - 55;
+        else s.hight += Jhin.charAt(offset) - 48;//得到高度
 
         offset++;
         if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z')
-            s.width = 16 * 16 * 16 * (Jhin.charAt(offset) - 87);
+            s.width = 16 * 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.width = 16 * 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += 16 * 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += 16 * 16 * (Jhin.charAt(offset) - 55);
         else s.width += 16 * 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += 16 * (Jhin.charAt(offset) - 55);
         else s.width += 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += Jhin.charAt(offset) - 87;
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.width += Jhin.charAt(offset) - 55;
         else s.width += Jhin.charAt(offset) - 48;//得到宽度
-        Width = s.width;//将宽度数据存进width中
 
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numComponents = 16 * (Jhin.charAt(offset) - 87);
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numComponents = 16 * (Jhin.charAt(offset) - 55);
         else s.numComponents = 16 * (Jhin.charAt(offset) - 48);
         offset++;
-        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numComponents += Jhin.charAt(offset) - 87;
+        if (Jhin.charAt(offset) >= 'a' && Jhin.charAt(offset) <= 'z') s.numComponents += Jhin.charAt(offset) - 55;
         else s.numComponents += Jhin.charAt(offset) - 48;
 
         for (int i = 0; i < 3; i++) {
@@ -130,20 +170,26 @@ public class JPEGs {
                 s.crTable = Jhin.charAt(offset) - 48;//得到cr表号
             }
         }
-        if(s.ySample/s.cbSample==1){
-            sampling=444;
+        if (s.ySample/s.cbSample==1) {
+            s.sampling = 444;
         }
-        else if (s.ySample/s.cbSample==2&&s.crSample==0){
-            sampling=420;
-        }
-        else{
-            sampling=422;
-        }
+            if (s.ySample/s.cbSample==1&&s.crSample!=0){
+                s.sampling=422;
+            }
+            else {
+                s.sampling=420;
+            }
 
+        return s;
     }
-        /**
-         * 读取SOF0段数据获取图片信息结束
-         */
+
+    /**使用时:
+     *      JPEGs jpeGs = new JPEGs();
+     *      jpeGs.translate(jpeGs.byte2string(jpeGs.image2byte("D://4.jpg")));
+     *      System.out.println(jpeGs.translate(jpeGs.byte2string(jpeGs.image2byte("D://4.jpg"))).width );
+     */
+
+
 
         /**
          * 构造器获取图片的huffman表和DCT数据
@@ -221,6 +267,13 @@ public class JPEGs {
     public static void main(String[] args) {
         String fileName = "1";
         JPEGs jpegs = new JPEGs("E:/test/"+fileName+ ".jpg");
+        //处理图片数据过程
+        JPEGs jjp = new JPEGs("E:/test/"+fileName+ ".jpg");
+        jjp.translate(jjp.byte2string(jjp.image2byte("E:/test/"+fileName+ ".jpg")));
+        Heigh=jjp.translate(jjp.byte2string(jjp.image2byte("E:/test/"+fileName+ ".jpg"))).hight;
+        Width=jjp.translate(jjp.byte2string(jjp.image2byte("E:/test/"+fileName+ ".jpg"))).width;
+        Sampling=jjp.translate(jjp.byte2string(jjp.image2byte("E:/test/"+fileName+ ".jpg"))).sampling;
+
     }
     /**
      * 仅异或DC系数
