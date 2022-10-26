b2137a37b41243db47c643882b36c350be78ef58
ReubenRogar <3186664089@qq.com>
2022-10-26 16:21:01 +0800

    实现两次解密方法

--- src/cn/hitwh/Encrypt/NewTypeEncrypt.java
+++ src/cn/hitwh/Encrypt/NewTypeEncrypt.java
@@ -17,11 +17,9 @@ public class NewTypeEncrypt {
      * 依据dct数据建立密钥
      * @param d dct数据
      * @param k 初始密钥
-     * @throws NoSuchAlgorithmException
      */
     public NewTypeEncrypt(ArrayList<int[]> d, KeyXU k) throws NoSuchAlgorithmException {
         DCTs = d;
-        KeyXU originKey = k;
 
         int[] mount = new int[64];
         int[] dct;
@@ -75,7 +73,7 @@ public class NewTypeEncrypt {
             hashFeature = hashFeature.substring(9);
             addUKey += ""+count;
         }
-        finalKey = new KeyXU(  Double.parseDouble(""+ originKey.x+addXKey),Double.parseDouble(""+ originKey.u+addUKey));
+        finalKey = new KeyXU(  Double.parseDouble(""+ k.x+addXKey),Double.parseDouble(""+ k.u+addUKey));
     }
 
 
@@ -149,7 +147,8 @@ public class NewTypeEncrypt {
      * @param iterations 迭代次数
      * @param resetInterval DCT重置间隔
      */
-    public void DCIterativeScramble(int iterations, int resetInterval, int max) {
+    public int DCIterativeScramble(int iterations, int resetInterval, int max) {
+        int count = 0;
         for (int group = 1; group <= iterations; group++) {
             //置乱序列初始化
             double[] scrambles = new double[(int) Math.ceil((double) DCTs.size() / (2 * group))];
@@ -192,6 +191,7 @@ public class NewTypeEncrypt {
                         }//end if
                     }//end for
                     //进行置乱
+                    count++;
                     for(int index = start1;index <= end1;index++){
                         int temp = DCTs.get(index)[0];
                         DCTs.get(index)[0] = DCTs.get(index + group)[0];
@@ -208,15 +208,109 @@ public class NewTypeEncrypt {
                 }//end for
             }//end for
         }//end for
+        return count;
     }
 
+    /**
+     * DCC迭代解密
+     * @param max 图片允许的DCC的位数
+     * @param iterations 迭代次数
+     * @param resetInterval DCT重置间隔
+     */
+    public int DCIterativeDecode(int iterations, int resetInterval, int max){
+        int count = 0;
+        for(int group = iterations; group >= 1;group--){
+            //置乱序列初始化
+            double[] scrambles = new double[(int) Math.ceil((double) DCTs.size() / (2 * group))];
+            scrambles[0] = finalKey.x;
+            for (int i = 1; i < scrambles.length; i++) {
+                scrambles[i] = finalKey.u * scrambles[i - 1] * (1 - scrambles[i - 1]);//x(n+1) = u * x(n) * (1 - x(n))
+            }
+            Arrays.sort(scrambles);
+            //处理分组
+            int lastDC = 0;//记录到start1 - 1的真值
+            int value;//记录假如置乱后某位上的真值
+            int lastDCSite = (scrambles.length - 2)*2*group - 1;
+            int countStart;
+            if(resetInterval == 0){
+                countStart = 0;
+            }else{
+                countStart = lastDCSite / resetInterval *resetInterval;
+            }
+            while (countStart <= lastDCSite){
+                lastDC +=DCTs.get(countStart++)[0];
+            }
+            RES:
+            for(int i = scrambles.length - 2;i >= 0;i--) {
+                String s = String.valueOf(scrambles[i]);
+                int c = Integer.parseInt(s.substring(s.length() - 1));
+                //以置乱比特流确定是否还原，奇数‘1’则还原
+                if (c % 2 != 0) {
+                    int start1 = i * 2 * group;
+                    int end1 = start1 + group - 1;
+                    int start2 = end1 + 1;
+                    int end2 = start2 + group - 1;
+                    value = lastDC;
+                    for (int index = start1; index <= end2; index++) {
+                        int real = index > end1 ? index - group : index + group;
+                        if (resetInterval != 0 && index % resetInterval == 0) {
+                            value = DCTs.get(real)[0];
+                        } else {
+                            value += DCTs.get(real)[0];
+                        }
+                        //超过huf表示范围
+                        if (Math.abs(value) >= 1 << max) {
+                            //设置lastDC
+                            for (int x = i * 2 * group - 1; x >= (i - 1) * 2 * group; x--) {
+                                if (resetInterval != 0 && x % resetInterval == 0) {
+                                    if(lastDC != DCTs.get(x)[0])throw new RuntimeException("带RST图片lastDC计算错误");
+                                    lastDCSite = (i - 1) * 2 * group - 1;
+                                    lastDC = 0;
+                                    countStart = lastDCSite / resetInterval * resetInterval;
+                                    while (countStart <= lastDCSite) {
+                                        lastDC += DCTs.get(countStart++)[0];
+                                    }
+                                    continue RES;
+                                } else {
+                                    lastDC -= DCTs.get(x)[0];
+                                }
+                            }//end for
+                            continue RES;
+                        }//end if
+                    }//end for
+                    //进行还原
+                    count++;
+                    for (int index = start1; index <= end1; index++) {
+                        int temp = DCTs.get(index)[0];
+                        DCTs.get(index)[0] = DCTs.get(index + group)[0];
+                        DCTs.get(index + group)[0] = temp;
+                    }
+                }//end if
+                if(i > 0)
+                for (int x = i * 2 * group - 1; x >= (i - 1) * 2 * group; x--) {
+                    if (resetInterval != 0 && x % resetInterval == 0) {
+                        lastDCSite = (i - 1) * 2 * group - 1;
+                        lastDC = 0;
+                        countStart = lastDCSite / resetInterval * resetInterval;
+                        while (countStart <= lastDCSite) {
+                            lastDC += DCTs.get(countStart++)[0];
+                        }
+                        break;
+                    } else {
+                        lastDC -= DCTs.get(x)[0];
+                    }
+                }//end for
+            }//end Res for
+        }//end for
+        return count;
+    }
 
     /**
      * 相同游程ACC全局加密
      */
     public void ACRunGroupScramble(){
-        ArrayList<NonZeroAC> runZero = new ArrayList<>();
-        ArrayList<NonZeroAC> runOne = new ArrayList<>();
+        ArrayList<SortFactor> runZero = new ArrayList<>();
+        ArrayList<SortFactor> runOne = new ArrayList<>();
         ArrayList<Point> runZeroSite = new ArrayList<>();
         ArrayList<Point> runOneSite = new ArrayList<>();
         //获取游程为1或0非0AC系数的信息
@@ -227,11 +321,11 @@ public class NewTypeEncrypt {
                 if(dct[i] != 0){
                     switch (i - last){
                         case 0:
-                            runZero.add(new NonZeroAC(dct[i]));
+                            runZero.add(new SortFactor(dct[i]));
                             runZeroSite.add(new Point(index,i));
                             break;
                         case 1:
-                            runOne.add(new NonZeroAC(dct[i]));
+                            runOne.add(new SortFactor(dct[i]));
                             runOneSite.add(new Point(index,i));
                             break;
                     }//end switch
@@ -239,16 +333,16 @@ public class NewTypeEncrypt {
                 }//end if
             }//end for
         }//end for
-        runZero.get(0).id = finalKey.x;
-        runOne.get(0).id = finalKey.x;
+        runZero.get(0).TAG = finalKey.x;
+        runOne.get(0).TAG = finalKey.x;
         for(int i = 1;i < runZero.size();i++){
-            runZero.get(i).id = finalKey.u * runZero.get(i-1).id * (1 - runZero.get(i-1).id);//x(n+1) = u * x(n) * (1 - x(n))
+            runZero.get(i).TAG = finalKey.u * runZero.get(i-1).TAG * (1 - runZero.get(i-1).TAG);//x(n+1) = u * x(n) * (1 - x(n))
         }
         for(int i = 1;i < runOne.size();i++){
-            runOne.get(i).id = finalKey.u * runOne.get(i-1).id * (1 - runOne.get(i-1).id);//x(n+1) = u * x(n) * (1 - x(n))
+            runOne.get(i).TAG = finalKey.u * runOne.get(i-1).TAG * (1 - runOne.get(i-1).TAG);//x(n+1) = u * x(n) * (1 - x(n))
         }
-        runZero.sort((o1, o2) -> o1.id > o2.id? 1 : -1);
-        runOne.sort((o1, o2) -> o1.id > o2.id? 1 : -1);
+        runZero.sort((o1, o2) -> o1.TAG > o2.TAG ? 1 : -1);
+        runOne.sort((o1, o2) -> o1.TAG > o2.TAG ? 1 : -1);
         for(int i = 0;i < runZeroSite.size();i++){
             DCTs.get(runZeroSite.get(i).x)[runZeroSite.get(i).y] = runZero.get(i).value;
         }
@@ -261,19 +355,78 @@ public class NewTypeEncrypt {
      * 相同游程ACC全局解密
      */
         public void ACRunGroupDecode(){
-
+            ArrayList<SortFactor> runZero = new ArrayList<>();
+            ArrayList<SortFactor> runOne = new ArrayList<>();
+            ArrayList<Point> runZeroSite = new ArrayList<>();
+            ArrayList<Point> runOneSite = new ArrayList<>();
+            //获取游程为1或0非0AC系数的信息
+            for(int index = 0;index < DCTs.size();index++){
+                int[] dct = DCTs.get(index);
+                int last = 1;
+                for (int i = 1;i < 64;i++){
+                    if(dct[i] != 0){
+                        switch (i - last){
+                            case 0:
+                                runZero.add(new SortFactor());
+                                runZeroSite.add(new Point(index,i));
+                                break;
+                            case 1:
+                                runOne.add(new SortFactor());
+                                runOneSite.add(new Point(index,i));
+                                break;
+                        }//end switch
+                        last = i;
+                    }//end if
+                }//end for
+            }//end for
+            runZero.get(0).TAG = finalKey.x;
+            runOne.get(0).TAG = finalKey.x;
+            for(int i = 1;i < runZero.size();i++){
+                runZero.get(i).TAG = finalKey.u * runZero.get(i-1).TAG * (1 - runZero.get(i-1).TAG);//x(n+1) = u * x(n) * (1 - x(n))
+                runZero.get(i).id = i;
+            }
+            for(int i = 1;i < runOne.size();i++){
+                runOne.get(i).TAG = finalKey.u * runOne.get(i-1).TAG * (1 - runOne.get(i-1).TAG);//x(n+1) = u * x(n) * (1 - x(n))
+                runOne.get(i).id = i;
+            }
+            runZero.sort((o1, o2) -> o1.TAG > o2.TAG ? 1 : -1);
+            runOne.sort((o1, o2) -> o1.TAG > o2.TAG ? 1 : -1);
+            for(int i = 0;i < runZeroSite.size();i++){
+                runZero.get(i).value = DCTs.get(runZeroSite.get(i).x)[runZeroSite.get(i).y]  ;
+            }
+            for(int i = 0;i < runOneSite.size();i++){
+                runOne.get(i).value = DCTs.get(runOneSite.get(i).x)[runOneSite.get(i).y]  ;
+            }
+            runZero.sort((o1,o2) -> o1.id > o2.id ? 1 : -1);
+            runOne.sort((o1,o2) -> o1.id > o2.id ? 1 : -1);
+            for(int i = 0;i < runZeroSite.size();i++){
+                DCTs.get(runZeroSite.get(i).x)[runZeroSite.get(i).y] = runZero.get(i).value;
+            }
+            for(int i = 0;i < runOneSite.size();i++){
+                DCTs.get(runOneSite.get(i).x)[runOneSite.get(i).y] = runOne.get(i).value;
+            }
     }
+
+
 }
 
 /**
  * 存储非0AC系数的信息
  */
-class NonZeroAC{
-    public double id;
+class SortFactor {
+    public double TAG;
     public int value;
+    public int id;
 
-    public NonZeroAC(int value){
-        this.id = 0;
+    public SortFactor(int value){
+        this.TAG = 0;
         this.value = value;
+        this.id = 0;
+    }
+
+    public SortFactor(){
+        this.TAG = 0;
+        this.value = 0;
+        this.id = 0;
     }
 }
